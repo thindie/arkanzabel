239fc1bc6dd0b8fb0f8add9c93836fdbf853c1bf
Alexey Iuzhakov <iuzhakov.aleksei.ial@gmail.com>
2026-03-21 11:44:21 +0200

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose)
+}
+
+android {
+  namespace = "com.thindie.rknzbl"
+  compileSdk {
+    version = release(36)
+  }
+
+  defaultConfig {
+    applicationId = "com.thindie.rknzbl"
+    minSdk = 24
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+  }
+  buildFeatures {
+    compose = true
+  }
+}
+
+dependencies {
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.graphics)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.compose.material3)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  debugImplementation(libs.androidx.compose.ui.tooling)
+}
