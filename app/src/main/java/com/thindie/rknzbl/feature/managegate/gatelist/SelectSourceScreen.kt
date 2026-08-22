a0ff2c0949f73b54bc2b76a094aa3456a4612308
thindie <thindie1@mail.ru>
2026-08-22 18:38:56 +0300

    [gatelist] Pass explicit ScreenScope param to AppScreen

--- app/src/main/java/com/thindie/rknzbl/feature/managegate/gatelist/SelectSourceScreen.kt
+++ app/src/main/java/com/thindie/rknzbl/feature/managegate/gatelist/SelectSourceScreen.kt
@@ -34,7 +34,7 @@ internal fun SelectSourceScreen(scope: ScreenScope<ScreenState, ScreenCommand>) 
   val screenState by scope.state.collectAsState()
   val context = LocalContext.current
   AppScreen(
-    scope = scope,
+    screenScope = scope,
     primary =
       Action(
         resRef = R.drawable.ic_arrow_back_24,
