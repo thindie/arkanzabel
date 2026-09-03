3987bbe62d00ae82799e30b7719b05e572b4c0d0
thindie <thindie1@mail.ru>
2026-09-03 03:17:36 +0300

    [home] Add LoadProfiles command

--- app/src/main/java/com/thindie/rknzbl/appfeatures/home/ui/ScreenCommand.kt
+++ app/src/main/java/com/thindie/rknzbl/appfeatures/home/ui/ScreenCommand.kt
@@ -3,6 +3,9 @@ package com.thindie.rknzbl.appfeatures.home.ui
 import com.thindie.engine.core.Command
 
 internal sealed interface ScreenCommand : Command {
+  /** Loads profiles and measures the best one on screen start. */
+  data object LoadProfiles : ScreenCommand
+
   data object Home : ScreenCommand
 
   data object New : ScreenCommand
